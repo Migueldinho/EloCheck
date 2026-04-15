a47e601be81f9058aa65bce06883c2fee21af50f
Miguel Angel Gutierrez Vera <mig.gutierrezv@duocuc.cl>
2026-04-15 13:38:54 -0400

    Add model
    Se agregaron nuevos modelos

---
+++ src/main/java/com/duoc/EloCheck/model/Equipo.java
@@ -0,0 +1,38 @@
+package com.duoc.EloCheck.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "equipos")
+public class Equipo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @NotBlank
+    private String nombreEquipo;
+
+    @NotNull
+    private Integer ram;
+
+    @NotBlank
+    private String grafica;
+
+    @NotBlank
+    private String procesador;
+
+    @NotBlank
+    private String elo;
+
+    @Id
+    private Integer idUsuario;
+}
---
+++ src/main/java/com/duoc/EloCheck/model/Usuario.java
@@ -0,0 +1,31 @@
+package com.duoc.EloCheck.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "usuarios")
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    private Integer id;
+
+    @NotBlank
+    private String nombre;
+
+    @NotBlank
+    private String clave;
+
+    @NotBlank
+    private String email;
+
+}
